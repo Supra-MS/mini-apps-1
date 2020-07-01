1f01f6b51e0017b4e9a7fdbbbe274b666a765dee
Supra-MS <suprabharathi.m.s@gmail.com>
2020-06-30 22:19:55 -0700

    Add svg work-around

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,21 +1,48 @@
+/* SVG */
+const circle = `<svg width="53" height="55" viewBox="0 0 53 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d)">
+<path d="M46.5 23.5C46.5 35.2015 37.4444 44.5 26.5 44.5C15.5556 44.5 6.5 35.2015 6.5 23.5C6.5 11.7985 15.5556 2.5 26.5 2.5C37.4444 2.5 46.5 11.7985 46.5 23.5Z" stroke="#F27404" stroke-width="5"/>
+</g>
+<defs>
+<filter id="filter0_d" x="0" y="0" width="53" height="55" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+</filter>
+</defs>
+</svg>`;
+
 const cross = `<svg width="50" height="42" viewBox="0 0 50 42" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3 2L21.587 17.2331L48 38.8801M3 38.8801C3 40.8471 33 18.118 48 6.50757" stroke="#BEDB38" stroke-width="5"/>
 </svg>`;
+
+
 
 let playerTurn;
 
 let playerScores = {
   'X': {
     'name': 'x',
-    'score': 0
+    'score': 0,
+    'color': '#bedb38'
   },
 
   'O': {
     'name': 'o',
+    'score': 0,
+    'color': '#f27404'
+  },
+  'ties': {
     'score': 0
   }
 };
 
+
+/* Get elements */
 let cells = Array.from(document.getElementsByClassName('cell'));
 let winnerMsg = document.getElementsByClassName('winnermsg');
 let winnerPanel = document.getElementsByClassName('winner-panel');
@@ -23,7 +50,12 @@ let playerx = document.getElementsByClassName('playerx');
 let playero = document.getElementsByClassName('playero');
 let pxscore = document.getElementsByClassName('pxscore');
 let poscore = document.getElementsByClassName('poscore');
+let tiescore = document.getElementsByClassName('tiescore');
+let playagain = document.getElementById('playagain');
+let board = document.getElementById('board');
+let reset = document.getElementById('reset');
 
+/* On Initial Page load */
 function init() {
   document.winner = null;
   playerTurn = 'X';
@@ -40,35 +72,41 @@ function init() {
 function nextMove(cell) {
   if (document.winner !== null) {
     return;
-  } else
-  if (cell.textContent === '') {
+  } else if (cell.innerText === '') {
     cell.textContent = playerTurn;
-    switchPlayer();
+    switchPlayer(cell);
   } else {
     console.log('Cell is occupied');
   }
 }
 
-function switchPlayer() {
+function switchPlayer(cell) {
   if (checkWinner(playerTurn)) {
+    cell.style.color = playerScores[playerTurn]['color'];
     console.log('winner msg');
     winnerPanel[0].classList.remove('invisible');
     winnerPanel[0].className += ' visible';
     winnerMessage(playerTurn);
     document.winner = playerTurn;
   } else if (checkTie()) {
+    cell.style.color = playerScores[playerTurn]['color'];
     console.log('tie msg');
     winnerPanel[0].classList.remove('invisible');
     winnerPanel[0].className += ' visible';
     winnerMsg[0].textContent = `It's a Tie. Play again!!`;
+    playerScores['ties']['score']++;
+    tiescore[0].textContent = playerScores['ties']['score'];
+
   } else if (playerTurn === 'X') {
     console.log('X turn');
+    cell.style.color = '#bedb38';
     playerTurn = 'O';
     playero[0].className += ' active';
     playerx[0].classList.remove('active');
   } else {
     console.log('O turn');
     playerTurn = 'X';
+    cell.style.color = '#f27404';
     playerx[0].className += ' active';
     playero[0].classList.remove('active');
   }
@@ -81,13 +119,14 @@ function checkWinner(move) {
 		|| checkCell(0, 4, 8, move) || checkCell(2, 4, 6, move)) {
 
 		result = true;
-	}
+  }
+
 	return result;
 }
 
 function checkCell(a, b, c, move) {
 	var result = false;
-	if (getCell(a) === move && getCell(b) === move && getCell(c) ===move) {
+	if (getCell(a) === move && getCell(b) === move && getCell(c) === move) {
 		result = true;
 	}
 	return result;
@@ -95,7 +134,7 @@ function checkCell(a, b, c, move) {
 }
 
 function getCell(number) {
-	return document.getElementById(number.toString()).textContent;
+	return document.getElementById('' + number).innerText;
 }
 
 function clearCell(cellValue) {
@@ -105,7 +144,6 @@ function clearCell(cellValue) {
 function winnerMessage(player) {
   winnerMsg[0].textContent = `Player ${player} Wins 😀!'`;
   playerScores[player].score++;
-  console.log(playerScores[player].score);
   if (player === 'X') {
     pxscore[0].textContent = playerScores[player].score;
   } else {
@@ -127,26 +165,13 @@ reset.addEventListener('click', function() {
   init();
   winnerPanel[0].classList.remove('visible');
   winnerPanel[0].className += ' invisible';
-  console.log('reset');
+  pxscore[0].textContent = playerScores['X'].score = 0;
+  poscore[0].textContent = playerScores['O'].score = 0;
+  tiescore[0].textContent = playerScores['ties'].score = 0;
 });
 
 
-/* SVG */
 
 
-const circle = `<svg width="53" height="55" viewBox="0 0 53 55" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d)">
-<path d="M46.5 23.5C46.5 35.2015 37.4444 44.5 26.5 44.5C15.5556 44.5 6.5 35.2015 6.5 23.5C6.5 11.7985 15.5556 2.5 26.5 2.5C37.4444 2.5 46.5 11.7985 46.5 23.5Z" stroke="#F27404" stroke-width="5"/>
-</g>
-<defs>
-<filter id="filter0_d" x="0" y="0" width="53" height="55" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
-<feOffset dy="4"/>
-<feGaussianBlur stdDeviation="2"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
-</filter>
-</defs>
-</svg>`;
+
+
