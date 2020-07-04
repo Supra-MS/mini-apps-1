2e6084254cd419af1a908f2980e7060cdd8c6410
Supra-MS <suprabharathi.m.s@gmail.com>
2020-07-04 13:08:09 -0700

    Link to download the csv results

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -116,5 +116,21 @@ $(() => {
     endpoint = `${serverUrl}/json2csv`;
   });
 
+  $('#download').on('click', (e) => {
+    endpoint = '';
+    let csvResult = $('#result')[0].value;
+    console.log('Download result: ', csvResult);
+
+    const blob = new Blob([csvResult], { type: 'text/csv' });
+    console.log('Blob: ', blob);
+    const url = window.URL.createObjectURL(blob);
+    const a  = document.createElement('a');
+    a.setAttribute('hidden', '');
+    a.setAttribute('href', url);
+    a.setAttribute('download', 'csvDataResults');
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  });
 
 });
