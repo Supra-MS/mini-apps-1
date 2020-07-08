bffd9740fcc741b499da780a2edda2feadfb793f
Supra-MS <suprabharathi.m.s@gmail.com>
2020-07-08 15:13:25 -0700

    Initial setup

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,12 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h5>React Checkout Page!!</h5>
+      </div>
+    )
+  }
+}
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3003;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`**** Server is listening on http://localhost:${port} ****`);
+});
+
 
 
